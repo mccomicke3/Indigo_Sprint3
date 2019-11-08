7153a67de9728f07a30e3b97684e33475a80270c
James <jyee0012@gmail.com>
2019-11-07 21:00:28 -0700

    Started Project

---
+++ Ultimate combo/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyScript : MonoBehaviour
+{
+    [SerializeField]
+    Enemy enemyInfo = null;
+    [SerializeField]
+    SpriteRenderer spriteRef = null;
+    [SerializeField]
+    Text enemyHealthText = null, attackSequenceText = null;
+
+    List<int> attackSequence = new List<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Attack(int type)
+    {
+        attackSequence.Add(type);
+        UpdateAttackSequence();
+    }
+    void UpdateAttackSequence()
+    {
+        string tempText = "Attacks: ";
+        for (int i = 0; i < attackSequence.Count;i++)
+        {
+            tempText += attackSequence[i];
+            if (i < attackSequence.Count)
+            {
+                tempText += "/";
+            }
+        }
+        if (attackSequenceText != null) attackSequenceText.text = tempText;
+    }
+}
