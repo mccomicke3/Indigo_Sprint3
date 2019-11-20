069960f4fd41f515ddca6aa95c601dc9656614dc
James <jyee0012@gmail.com>
2019-11-19 17:04:10 -0700

    added random to enemy

--- Ultimate combo/Assets/Scripts/Enemy.cs
+++ Ultimate combo/Assets/Scripts/Enemy.cs
@@ -5,10 +5,26 @@ using UnityEngine;
 [CreateAssetMenu()]
 public class Enemy : ScriptableObject
 {
+    public static string[] nameList = { "Tom", "Geoff", "Jack", "Jeremy", "Trevor", "Ryan", "Bob", "Billy", "Kevin", "Jeff", "Steve", "Sarah", "Andrew", "Cindy", "Vincent", "Katie", "Lisa", "Eric", "Brock", "Brocc", "Ash", "Ashley" };
     public string enemyName = "Brocc";
     public int level = 1;
     public int[] reactions = new int[4];
     public Sprite enemyImage = null;
+    public Enemy(int level = 0)
+    {
+        enemyName = nameList[Random.Range(0,nameList.Length)];
+        int reactionAmount = 4 + (level);
+        reactions = NewReactions(reactionAmount);
+    }
+    public int[] NewReactions(int length)
+    {
+        int[] newReactions = new int[length];
+        for (int i=0; i < length; i++)
+        {
+            newReactions[i] = Random.Range(0, 4);
+        }
+        return newReactions;
+    }
     // Start is called before the first frame update
     void Start()
     {
--- Ultimate combo/Assets/Scripts/EnemyScript.cs
+++ Ultimate combo/Assets/Scripts/EnemyScript.cs
@@ -10,10 +10,17 @@ public class EnemyScript : MonoBehaviour
     [SerializeField]
     SpriteRenderer spriteRef = null;
     [SerializeField]
-    Text enemyHealthText = null, attackSequenceText = null;
+    Text enemyHealthText = null, attackSequenceText = null, enemyNameText = null;
 
+    int prevAttack = -1;
     List<int> attackSequence = new List<int>();
+    [Header("Test")]
+    [SerializeField]
+    bool test = false;
+    [SerializeField]
+    KeyCode testKey = KeyCode.T;
 
+    float testDelay = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,23 +30,41 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if ((test && Input.GetKey(testKey)) && testDelay < Time.time)
+        {
+            enemyInfo = new Enemy(Random.Range(0,5));
+            if (enemyNameText != null) enemyNameText.text = enemyInfo.enemyName;
+            if (enemyHealthText != null) enemyHealthText.text = "HP: " + enemyInfo.reactions.Length;
+            testDelay = Time.time + 0.5f;
+        }
     }
-    public void Attack(int type)
+    public void AddCombo(int type)
     {
+        if (type == prevAttack) return;
         attackSequence.Add(type);
         UpdateAttackSequence();
+        prevAttack = type;
+    }
+    public void ClearSequence()
+    {
+        attackSequence.Clear();
+        UpdateAttackSequence();
+        prevAttack = -1;
+    }
+    public void Attack()
+    {
+        Debug.Log("You attacked");
     }
     void UpdateAttackSequence()
     {
         string tempText = "Attacks: ";
         for (int i = 0; i < attackSequence.Count;i++)
         {
-            tempText += attackSequence[i];
-            if (i < attackSequence.Count)
+            if (i > 0)
             {
                 tempText += "/";
             }
+            tempText += attackSequence[i];
         }
         if (attackSequenceText != null) attackSequenceText.text = tempText;
     }
