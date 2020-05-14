aad538dab50f72ee58f236dff781d60bd4217829
silun-zhang <56770701+silun-zhang@users.noreply.github.com>
2020-05-14 16:58:23 -0600

    Added Title Scene and Music Script
    - Added list to switch music (with MPlay, MPause, and MChange)
    - Added title scene to switch scenes to the main scene
    - Background change in main scene

---
+++ Ultimate combo/Assets/Scripts/Music.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    private AudioSource source;
+    public AudioClip current;
+    public int currentnum;
+    public List<AudioClip> allmusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GameObject.Find("Audio Source"). GetComponent<AudioSource>();
+        source.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Used to change the music
+    void MChange(int soundnum)
+    {
+        currentnum = soundnum;
+        current = allmusic[soundnum];
+    }
+
+    // Used to play the music
+    void MPlay()
+    {
+        source.Play();
+    }
+
+    // Used to pause the music
+    void MPause()
+    {
+        source.Pause();
+    }
+}
